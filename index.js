4c97899539e7a1992c37f3d61a6f8830f71dd4de
Andy Bell <me@andy-bell.co.uk>
2018-03-30 21:03:18 +0100

    Basic scaffold added

---
+++ index.js
@@ -0,0 +1,10 @@
+// Run dotenv
+require('dotenv').config();
+
+const API_KEY = process.env.LAST_FM_API_KEY;
+
+const lastFmMostRecentTrack = () => {
+    return {};
+};
+
+export default lastFmMostRecentTrack();
